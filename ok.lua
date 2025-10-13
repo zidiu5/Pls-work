79ec17f9411e36cc3cf8b3df56a709ece1597789
zidiu5 <147642178+zidiu5@users.noreply.github.com>
2025-10-13 21:30:01 +0200

    Update ok.lua

--- ok.lua
+++ ok.lua
@@ -1104,8 +1104,8 @@ for _, category in pairs(EggsFolder:GetChildren()) do
                                 }
                             }
                             pcall(function()
-                                if Remotes:FindFirstChild("buy egg") and Remotes["buy egg"].InvokeServer then
-                                    Remotes["buy egg"]:InvokeServer(unpack(args))
+                                if Remotes:FindFirstChild("buyx egg") and Remotes["buyx egg"].InvokeServer then
+                                    Remotes["buyx egg"]:InvokeServer(unpack(args))
                                 end
                             end)
                         end
@@ -1463,7 +1463,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local SaveModule = require(ReplicatedStorage.Framework.Modules.Client:WaitForChild("4 | Save"))
 local Remotes = Workspace:WaitForChild("__THINGS"):WaitForChild("__REMOTES")
-local BuyEggRemote = Remotes:WaitForChild("buy egg")
+local BuyEggRemote = Remotes:WaitForChild("buyx egg")
 local DeletePetsRemote = Remotes:WaitForChild("delete several pets")
 
 local MiscTabButton = createTabButton("Misc")
